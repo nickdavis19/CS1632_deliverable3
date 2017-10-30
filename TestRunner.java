40899ef153fa3134c6e28bae1a360686d4465c22
erh74 <erh74@pitt.edu>
2017-10-30 01:23:26 -0400

    Add second half of tests to TestRunner

--- TestRunner.java
+++ TestRunner.java
@@ -12,6 +12,7 @@ public class TestRunner {
 	// ADD ANY MORE CLASSES YOU WISH TO TEST HERE
 	
 	classesToTest.add(TestReq1Thru5.class);
+	classToTest.add(Test5thru8.class);
 	
 	// For all test classes added, loop through and use JUnit
 	// to run them.
